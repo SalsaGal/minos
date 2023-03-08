ebb19a91391b235aaf156708de75cd3a435504b3
SalsaGal <fielddashos@gmail.com>
2023-03-08 23:01:37 +1100

    Add print

--- src/boot.zig
+++ src/boot.zig
@@ -1,16 +1,13 @@
 const uefi = @import("std").os.uefi;
 const string = @import("string.zig");
 
-var con_out: *uefi.protocols.SimpleTextOutputProtocol = undefined;
-var memory_buffer: [0xffff]u8 = undefined;
+pub var con_out: *uefi.protocols.SimpleTextOutputProtocol = undefined;
 
 pub fn main() void {
     con_out = uefi.system_table.con_out.?;
-
-    const text = string.Str16{ .chars = &[_:0]u16{ 'H', 'i' } };
-
     _ = con_out.reset(false);
-    _ = con_out.outputString(text.chars);
+
+    string.print("Foo");
 
     _ = uefi.system_table.boot_services.?.stall(5_000_000);
 }
--- src/string.zig
+++ src/string.zig
@@ -1,3 +1,11 @@
+const boot = @import("boot.zig");
+
 pub const Str16 = struct {
     chars: [:0]const u16,
 };
+
+pub fn print(str: [:0]const u8) void {
+    for (str) |c| {
+        _ = boot.con_out.outputString(&[_:0]u16{c});
+    }
+}
