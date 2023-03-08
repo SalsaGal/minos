0e8d24d97ef878b12c7361ccad9087eea7569a08
SalsaGal <fielddashos@gmail.com>
2023-03-08 22:48:53 +1100

    Add memory_buffer

--- src/boot.zig
+++ src/boot.zig
@@ -1,8 +1,11 @@
 const uefi = @import("std").os.uefi;
 const string = @import("string.zig");
 
+var con_out: *uefi.protocols.SimpleTextOutputProtocol = undefined;
+var memory_buffer: [0xffff]u8 = undefined;
+
 pub fn main() void {
-    const con_out = uefi.system_table.con_out.?;
+    con_out = uefi.system_table.con_out.?;
 
     const text = string.Str16{ .chars = &[_:0]u16{ 'H', 'i' } };
 
