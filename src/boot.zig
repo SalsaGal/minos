7ae2fdb6ac5fbce1783a1fe9d47c39ca93028508
SalsaGal <fielddashos@gmail.com>
2023-03-09 22:12:19 +1100

    Make con_out accessible outside of main

--- src/boot.zig
+++ src/boot.zig
@@ -1,9 +1,11 @@
 const std = @import("std");
 const uefi = std.os.uefi;
 
+var con_out: *uefi.protocols.SimpleTextOutputProtocol = undefined;
+
 pub fn main() void {
     const boot_services = uefi.system_table.boot_services.?;
-    const con_out = uefi.system_table.con_out.?;
+    con_out = uefi.system_table.con_out.?;
     _ = con_out.reset(false);
     _ = con_out.outputString(&[_:0]u16{ 'H', 'i' });
 
