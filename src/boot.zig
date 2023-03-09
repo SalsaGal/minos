9a3830c0a058bc0e1b58ebc112c9d7b751649089
SalsaGal <fielddashos@gmail.com>
2023-03-09 22:17:13 +1100

    Add print

--- src/boot.zig
+++ src/boot.zig
@@ -5,9 +5,18 @@ var con_out: *uefi.protocols.SimpleTextOutputProtocol = undefined;
 
 pub fn main() void {
     const boot_services = uefi.system_table.boot_services.?;
+
     con_out = uefi.system_table.con_out.?;
     _ = con_out.reset(false);
-    _ = con_out.outputString(&[_:0]u16{ 'H', 'i' });
+
+    print("foo\r\n");
+    print("Bar");
 
     _ = boot_services.stall(5_000_000);
+}
+
+fn print(str: []const u8) void {
+    for (str) |char| {
+        _ = con_out.outputString(&[_:0]u16{char});
+    }
 }
