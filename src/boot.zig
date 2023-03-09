46367cb58441722c2939477e0a2aed8423f3648e
SalsaGal <fielddashos@gmail.com>
2023-03-09 22:08:54 +1100

    Print test message

--- src/boot.zig
+++ src/boot.zig
@@ -1,4 +1,11 @@
 const std = @import("std");
 const uefi = std.os.uefi;
 
-pub fn main() void {}
+pub fn main() void {
+    const boot_services = uefi.system_table.boot_services.?;
+    const con_out = uefi.system_table.con_out.?;
+    _ = con_out.reset(false);
+    _ = con_out.outputString(&[_:0]u16{ 'H', 'i' });
+
+    _ = boot_services.stall(5_000_000);
+}
